89ef4b8d35db1d21021f7c62255c77969f267e69
Administrator <hankin_06@outlook.com>
2020-08-05 19:05:26 +0800

    linked list

--- 408/src/main/java/linkedlist/single/SingleLinkedListDemo.java
+++ 408/src/main/java/linkedlist/single/SingleLinkedListDemo.java
@@ -8,17 +8,30 @@ package linkedlist.single;
  */
 public class SingleLinkedListDemo {
     public static void main(String[] args) {
-        HeroNode hero1 =new HeroNode(1,"宋江","及时雨");
-        HeroNode hero2 =new HeroNode(2,"卢俊义","玉麒麟");
-        HeroNode hero3 =new HeroNode(3,"吴用","智多星");
-        HeroNode hero4 =new HeroNode(4,"公孙胜","入云龙");
-        HeroNode hero5 =new HeroNode(5,"关胜","大刀");
-        SingleLinkedList singleLinkedList =new SingleLinkedList();
-        singleLinkedList.addNode(hero1);
+        HeroNode hero1 = new HeroNode(1, "宋江", "及时雨");
+        HeroNode hero2 = new HeroNode(2, "卢俊义", "玉麒麟");
+        HeroNode hero3 = new HeroNode(3, "吴用", "智多星");
+        HeroNode hero4 = new HeroNode(4, "公孙胜", "入云龙");
+        HeroNode hero5 = new HeroNode(5, "关胜", "大刀");
+        HeroNode hero7 = new HeroNode(6, "秦明", "霹雳火");
+        HeroNode hero6 = new HeroNode(6, "林冲", "豹子头");
+        SingleLinkedList singleLinkedList = new SingleLinkedList();
+       /* singleLinkedList.addNode(hero1);
         singleLinkedList.addNode(hero2);
         singleLinkedList.addNode(hero3);
         singleLinkedList.addNode(hero4);
         singleLinkedList.addNode(hero5);
+        singleLinkedList.showLinkedList();*/
+
+        singleLinkedList.addNodeByOrder(hero2);
+        singleLinkedList.addNodeByOrder(hero5);
+//        singleLinkedList.addNodeByOrder(hero1);
+        singleLinkedList.addNodeByOrder(hero4);
+        singleLinkedList.addNodeByOrder(hero3);
+        singleLinkedList.addNodeByOrder(hero7);
+
+        singleLinkedList.modifyByNo(hero1);
+        singleLinkedList.deleteNode(hero1);
         singleLinkedList.showLinkedList();
 
     }
@@ -46,6 +59,55 @@ class SingleLinkedList {
         temp.next = node;
     }
 
+    public void deleteNode(HeroNode node){
+        HeroNode temp = head;
+        while (temp.next != null&&temp.next.no!=node.no) {
+            temp = temp.next;
+        }
+        if (temp.next != null){
+            temp.next= temp.next.next;
+        }else {
+            System.out.println("No this node");
+        }
+    }
+
+    /**
+     * 按顺序来添加节点
+     *
+     * @param node
+     */
+    public void addNodeByOrder(HeroNode node) {
+        HeroNode temp = head;
+        while (temp.next != null && temp.next.no < node.no) {
+            temp = temp.next;
+        }
+        if (temp.next != null && temp.next.no == node.no) {
+            return;
+        }
+        HeroNode bigNode = temp.next;
+        temp.next = node;
+        node.next = bigNode;
+    }
+
+    /**
+     * 根据no 修改节点内容
+     * @param node
+     */
+    public void modifyByNo(HeroNode node) {
+        HeroNode temp = head;
+        while (temp.next != null&&temp.next.no!=node.no) {
+            temp = temp.next;
+        }
+        if (temp.next != null){
+            temp.next.nickName=node.nickName;
+            temp.next.name=node.name;
+        }else {
+            System.out.println("can not find it!");
+        }
+    }
+
+
+
     /**
      * 显示链表
      */
@@ -54,7 +116,7 @@ class SingleLinkedList {
             System.out.println("Empty");
         } else {
             HeroNode temp = head;
-            while (temp.next != null) {
+            while (temp != null) {
                 System.out.println(temp);
                 temp = temp.next;
             }

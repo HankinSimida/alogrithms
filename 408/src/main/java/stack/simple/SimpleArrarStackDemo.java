7e097e4356746642493a1b5f6657a7cc4ce88fb1
Administrator <hankin_06@outlook.com>
2020-08-16 12:16:32 +0800

    stack

--- 408/src/main/java/stack/simple/SimpleArrarStackDemo.java
+++ 408/src/main/java/stack/simple/SimpleArrarStackDemo.java
@@ -1,19 +1,66 @@
 package stack.simple;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Stack;
+
+import static java.lang.Character.isDigit;
+
 /**
  * @author hankin
  * @date 2020/8/15 22:16
  */
 public class SimpleArrarStackDemo {
     public static void main(String[] args) {
-        ArrayStack arrayStack =new ArrayStack(4);
+        /*ArrayStack arrayStack =new ArrayStack(4);
         arrayStack.push(15);
         arrayStack.push(16);
         arrayStack.push(17);
         arrayStack.push(18);
         arrayStack.list();
         arrayStack.pop();
-        arrayStack.list();
+        arrayStack.list();*/
+        String express = "-10+20*25";
+        char c = express.charAt(0);
+        char[] s={c};
+        int[] numArr = new int[3];
+        int k=0;
+        char[] charaOper = new char[2];
+        int t = 0;
+        String digitalStr = new String(s);
+        for (int i = 1; i < express.length(); i++) {
+            if (Character.isDigit(express.charAt(i))){
+                digitalStr+=express.charAt(i);
+                if (i==express.length()-1){
+                    numArr[k]=Integer.valueOf(digitalStr);
+                }
+            }else {
+                numArr[k]=Integer.valueOf(digitalStr);
+                k++;
+                digitalStr= "";
+                charaOper[t]=express.charAt(i);
+                t++;
+            }
+        }
+        Arrays.stream(numArr).forEach(x-> System.out.println(x+" "));
+        System.out.println(charaOper);
+        Stack<Integer> numNeedDealWith = new Stack<>();
+        Stack<Character> charNeedDealWith = new Stack<>();
+        for (int i = 0; i < charaOper.length; i++) {
+            if (i==0){
+                numNeedDealWith.push(numArr[i]);
+                charNeedDealWith.push(charaOper[i]);
+                numNeedDealWith.push(numArr[i+1]);
+            }
+            else {
+
+                int i1 = numArr[2] * numNeedDealWith.pop();
+                numNeedDealWith.push(i1);
+            }
+
+        }
+        int i = numNeedDealWith.pop() + numNeedDealWith.pop();
+        System.out.println(i);
     }
 }
 
